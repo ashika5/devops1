c1fcb75f53d71d57473ad5f2993b3eaf774b450a
ashika5 <94155259+ashika5@users.noreply.github.com>
2023-10-26 12:11:25 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
-public class Hello{
-public static void main(String[] args){
-System.out.println("helloworld");
-}
-}
+class hello
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
